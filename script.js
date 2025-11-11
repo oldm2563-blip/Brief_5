049f8c4e4a78ed5c28d902deca8d20d833e45994
oldman <oldm2563@email.com>
2025-11-11 23:05:33 +0100

    finish the flashcards and the js of em and their css

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
+
 const questioninput = document.getElementById("question");
 const answerinput = document.getElementById("answer");
 const btnofpower = document.getElementById("createflashcard");
 const cardcontainer = document.getElementById("flashcard");
+/*----------------------------------------------------------*/
+const categoryinput = document.getElementById("inputcategory");
+const addcategorybtn = document.getElementById("add-category-btn");
+const filter = document.getElementById("filter");
+
 
 let cards =JSON.parse(localStorage.getItem("flashcards")) || [];
+let categories = JSON.parse(localStorage.getItem("category")) || [];
+
+document.querySelector("#flashcard-form")?.addEventListener("submit", e => e.preventDefault());
 
 
-document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
-});
+addcategorybtn.addEventListener("click" , () => {
+    const newcategory = categoryinput.value.trim();
+    if(newcategory === ""){
+        alert("make sure to fill out the box");
+        return;
+    }
+    let exist = false;
+    for(let i = 0 ; i < filter.options.length ; i++){
+        if(newcategory === filter.options[i].value){
+            exist = true;
+            break;
+        }
+    }
+    if(exist){
+        alert("pick a diffrent name");
+        categoryinput.value = "";
+        return;
+    }
+
+
+    const option = document.createElement("option");
+    option.value = newcategory;
+    option.textContent = newcategory;
+    filter.appendChild(option);
+    
+
+    categories.push(newcategory)
+    localStorage.setItem("category" , JSON.stringify(categories))
+
+    categoryinput.value = "";
+
+})
+
+
+
 
 btnofpower.addEventListener("click" , ()=> {
     const question = questioninput.value.trim();
     const answer = answerinput.value.trim();
+    const drop = filter.value;
 
     if(question === "" || answer === ""){
         alert("the two must be filled");
@@ -20,6 +62,7 @@ btnofpower.addEventListener("click" , ()=> {
     }
     const card = document.createElement("div");
     card.classList.add("flashcard");
+    card.dataset.drop = drop;
     card.innerHTML = `
     <div class = "card-inner">
         <div class= "front">${question}</div>
@@ -30,7 +73,7 @@ btnofpower.addEventListener("click" , ()=> {
      card.classList.toggle("flipped")
  })
  cardcontainer.appendChild(card);
- cards.push({question, answer});
+ cards.push({question, answer, drop});
  localStorage.setItem("flashcards", JSON.stringify(cards));
 
  questioninput.value = "";
@@ -38,9 +81,18 @@ btnofpower.addEventListener("click" , ()=> {
 })
 
 window.addEventListener("DOMContentLoaded", () =>{
-    cards.forEach(({question, answer}) => {
+    categories.forEach(e => {
+        const option = document.createElement("option");
+        option.value = e;
+        option.textContent = e;
+        filter.appendChild(option);
+    })
+    
+
+    cards.forEach(({question, answer, drop}) => {
         const card = document.createElement("div");
         card.classList.add("flashcard");
+        card.dataset.drop = drop;
         card.innerHTML = `
             <div class="card-inner">
                 <div class="front">${question}</div>
@@ -53,5 +105,18 @@ window.addEventListener("DOMContentLoaded", () =>{
 })
 
 
+
+
+filter.addEventListener("change", ()=>{
+    const selected = filter.value;
+    document.querySelectorAll(".flashcard").forEach(card =>{
+        if(card.dataset.drop === selected){
+        card.style.display = "block"
+    }
+    else{
+        card.style.display = "none"
+    }
+    })
+})
 
 /*-------------------------------------------------------------------*/
