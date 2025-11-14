fc765192b474d0a592828dfc5dbac2c5fb3d1db2
oldman <oldm2563@email.com>
2025-11-14 10:47:52 +0100

    fixed the filter

--- script.js
+++ script.js
@@ -10,26 +10,26 @@ const filter = document.getElementById("filter");
 const optioned = document.getElementById("option")
 
 
-let cards =JSON.parse(localStorage.getItem("flashcards")) || [];
+let cards = JSON.parse(localStorage.getItem("flashcards")) || [];
 let categories = JSON.parse(localStorage.getItem("category")) || [];
 
 document.querySelector("#flashcard-form")?.addEventListener("submit", e => e.preventDefault());
 
 
-addcategorybtn.addEventListener("click" , () => {
+addcategorybtn.addEventListener("click", () => {
     const newcategory = categoryinput.value.trim();
-    if(newcategory === ""){
+    if (newcategory === "") {
         alert("make sure to fill out the box");
         return;
     }
     let exist = false;
-    for(let i = 0 ; i < filter.options.length ; i++){
-        if(newcategory === filter.options[i].value){
+    for (let i = 0; i < filter.options.length; i++) {
+        if (newcategory === filter.options[i].value) {
             exist = true;
             break;
         }
     }
-    if(exist){
+    if (exist) {
         alert("pick a diffrent name");
         categoryinput.value = "";
         return;
@@ -45,11 +45,11 @@ addcategorybtn.addEventListener("click" , () => {
     optionb.value = newcategory;
     optionb.textContent = newcategory;
     optioned.appendChild(optionb);
-    
-    
+
+
 
     categories.push(newcategory)
-    localStorage.setItem("category" , JSON.stringify(categories))
+    localStorage.setItem("category", JSON.stringify(categories))
 
     categoryinput.value = "";
 
@@ -58,12 +58,12 @@ addcategorybtn.addEventListener("click" , () => {
 
 
 
-btnofpower.addEventListener("click" , ()=> {
+btnofpower.addEventListener("click", () => {
     const question = questioninput.value.trim();
     const answer = answerinput.value.trim();
     const drop = filter.value;
 
-    if(question === "" || answer === ""){
+    if (question === "" || answer === "") {
         alert("the two must be filled");
         return;
     }
@@ -76,19 +76,19 @@ btnofpower.addEventListener("click" , ()=> {
         <div class= "back">${answer}</div>
     </div>
     `;
-     card.addEventListener("click" , () =>{
-     card.classList.toggle("flipped")
- })
- cardcontainer.appendChild(card);
- cards.push({question, answer, drop});
- localStorage.setItem("flashcards", JSON.stringify(cards));
+    card.addEventListener("click", () => {
+        card.classList.toggle("flipped")
+    })
+    cardcontainer.appendChild(card);
+    cards.push({ question, answer, drop });
+    localStorage.setItem("flashcards", JSON.stringify(cards));
 
- questioninput.value = "";
- answerinput.value = "";
+    questioninput.value = "";
+    answerinput.value = "";
 
 
 
-  const selected = optioned.value;
+    const selected = optioned.value;
     document.querySelectorAll(".flashcard").forEach(card => {
         if (selected === "all" || card.dataset.drop === selected) {
             card.style.display = "block";
@@ -98,7 +98,7 @@ btnofpower.addEventListener("click" , ()=> {
     });
 })
 
-window.addEventListener("DOMContentLoaded", () =>{
+window.addEventListener("DOMContentLoaded", () => {
     categories.forEach(e => {
         const option = document.createElement("option");
         option.value = e;
@@ -111,9 +111,9 @@ window.addEventListener("DOMContentLoaded", () =>{
         optioned.appendChild(optionb);
 
     })
-    
 
-    cards.forEach(({question, answer, drop}) => {
+
+    cards.forEach(({ question, answer, drop }) => {
         const card = document.createElement("div");
         card.classList.add("flashcard");
         card.dataset.drop = drop;
@@ -131,15 +131,15 @@ window.addEventListener("DOMContentLoaded", () =>{
 
 
 
-optioned.addEventListener("change", ()=>{
-    const selected = optioned.value;
-    document.querySelectorAll(".flashcard").forEach(card =>{
-        if(selected === "all" || card.dataset.drop === selected){
-        card.style.display = "block"
-    }
-    else{
-        card.style.display = "none"
-    }
+optioned.addEventListener("change", () => {
+    const selected = optioned.value||"all";
+    document.querySelectorAll(".flashcard").forEach(card => {
+        if (selected === "all" || card.dataset.drop === selected) {
+            card.style.display = "block"
+        }
+        else {
+            card.style.display = "none"
+        }
     })
 })
 
